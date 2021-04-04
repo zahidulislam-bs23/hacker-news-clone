296def12abb599d392c42a795e21fcaa0f180e8c
Zahidul Islam <zahidul@bs-23.net>
2021-04-04 14:19:25 +0600

    Core Module added with ErrorIntercp & LoggerServc

---
+++ src/app/@core/core.module.ts
@@ -0,0 +1,27 @@
+import { NgModule, Optional, SkipSelf } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { RouteReuseStrategy, RouterModule } from '@angular/router';
+
+import { ErrorHandlerInterceptor } from './http/error-handler-interceptor.service';
+
+@NgModule({
+  imports: [CommonModule, HttpClientModule, RouterModule],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ErrorHandlerInterceptor,
+      multi: true,
+    },
+  ],
+})
+export class CoreModule {
+  constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
+    // Import guard
+    if (parentModule) {
+      throw new Error(
+        `${parentModule} has already been loaded. Import Core module in the AppModule only.`
+      );
+    }
+  }
+}
